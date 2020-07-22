8dd20fff45619c0e5476368616955c0269c897b0
mervat <mervat.yosef.14@gmail.com>
2020-07-22 10:15:02 +0300

    completedTasks

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const todoList = document.querySelector(".todoList");
 const input = document.querySelector("#addList");
 const buttton = document.querySelector("#submit");
 const form = document.querySelector("#form");
+const completedTasks = document.querySelector(".completedTasks");
 
 form.addEventListener("submit", (event) => {
   event.preventDefault();
@@ -11,11 +12,17 @@ function creatToDo(task) {
   const li = document.createElement("li");
   const textnode = document.createTextNode(task);
   li.appendChild(textnode);
+
+  li.addEventListener("click", (e) => {
+    e.target.style.color = "red";
+    var el = li.cloneNode(true);
+    completedTasks.appendChild(el);
+  });
+
   return li;
 }
 
 buttton.addEventListener("click", () => {
-  console.log(input.value);
   let elemnt = creatToDo(input.value);
 
   todoList.appendChild(elemnt);
